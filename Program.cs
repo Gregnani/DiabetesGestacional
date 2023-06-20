0255c7a8c59473af50899359039d69192a7a05a5
gregnani <fernandou.gregnani@gmail.com>
2023-06-19 22:05:26 -0300

    Atividade concluida

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("--- Diabetes Gestacional ---\n");
+
+Console.Write("Digite o valor de glicemia (mg/dL):");
+double glicemia = Convert.ToDouble(Console.ReadLine()!);
+
+ConsoleColor corResposta = ConsoleColor.Red;
+
+
+if (glicemia < 92)
+
+    {
+    Console.ForegroundColor = corResposta;
+    Console.Write("Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
+    }
+
+    else if (glicemia >= 92 && glicemia < 126) 
+
+    {
+    Console.ForegroundColor = corResposta = ConsoleColor.Yellow ;
+    Console.Write("Comapatível com Diabettes Gestacional");
+    }
+
+    else 
+
+    {
+
+    Console.ForegroundColor = corResposta = ConsoleColor.Green;
+    Console.Write("Diabetes Mellitus na Gravizez");
+    
+    }
+
+Console.ResetColor();
+
+
